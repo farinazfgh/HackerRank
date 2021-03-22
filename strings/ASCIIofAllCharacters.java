0cd38b461101c303f637a76f8d0d20dd83a8f590
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2021-03-22 09:16:11 +0430

    polishing

---
+++ strings/ASCIIofAllCharacters.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class ASCIIofAllCharacters {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 255; i++) {
+            System.out.println("" + (char) i + ": " + i);
+        }
+    }
+}
---
+++ strings/SuperReducedString.java
@@ -0,0 +1,37 @@
+package  strings;
+
+public class SuperReducedString {
+    static String superReducedString(String s) {
+        String reducedStr = reduce(s);
+        while (reducedStr.length() < s.length()) {
+            s = reducedStr;
+            reducedStr = reduce(reducedStr);
+        }
+        return reducedStr;
+    }
+
+    static String reduce(String s) {
+        if (s == null || s.length() == 0) return "Empty String";
+        if (s.length() == 1) return s;
+        int count = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+            } else {
+                if (count % 2 == 1) sb.append(s.charAt(i));
+                count = 1;
+            }
+        }
+        if (count % 2 == 1) sb.append(s.charAt(s.length() - 1));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        //i<8 i=7
+        System.out.println(superReducedString("aaabccddd"));
+        System.out.println(superReducedString("aab"));
+        System.out.println(superReducedString("abba"));
+    }
+
+}
